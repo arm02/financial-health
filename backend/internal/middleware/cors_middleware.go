f581add03dec53bbb1c28d01744d84f30a69e7fd
Adrian <arm.adrian02@gmail.com>
2025-12-07 23:03:08 +0700

    feat: set cors by env

--- backend/internal/middleware/cors_middleware.go
+++ backend/internal/middleware/cors_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"financial-health/internal/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,7 +9,7 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://financial.adrianmilano.my.id")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", utils.GetEnv("ORIGIN", "http://localhost:4200"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Request-ID")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
