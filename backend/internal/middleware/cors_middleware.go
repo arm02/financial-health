48e69d255faa6137b0a58ef26015cb170ff719bb
Adrian <arm.adrian02@gmail.com>
2025-12-07 21:57:32 +0700

    feat: set data

--- backend/internal/middleware/cors_middleware.go
+++ backend/internal/middleware/cors_middleware.go
@@ -8,7 +8,7 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://financial.adrianmilano.my.id")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Request-ID")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://engine-financial.adrianmilano.my.id/api/v1',
   appName: 'Financial Health App',
   storage: {
     authData: 'auth_data'
